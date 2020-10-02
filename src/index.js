348cd9621459a29dfd6e191a31af19d22615d59d
Dima Kukhta <dima.kukhta.2001@mail.ru>
2020-10-02 19:08:09 +0300

    feat : passing all the test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+  let min = 0;
+  if (array < 1 || array === undefined ){
   return 0;
+  }
+  return Math.min.apply(null,array);
 }
 
 exports.max = function max (array) {
+  let max = 0;
+  if (array < 1 || array === undefined){
   return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
+  let avg = 0;
+  if (array < 1 || array === undefined){
   return 0;
+  }
+  return array.reduce((a, b) => (a + b)) / array.length;
 }
